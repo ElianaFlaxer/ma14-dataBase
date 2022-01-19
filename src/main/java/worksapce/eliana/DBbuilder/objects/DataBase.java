5c6516bf6da64bc2bb1e7e69ccc9d1563be3f631
ElianaFlaxer <flaxereliana@gmail.com>
2022-01-19 18:17:05 +0200

    added dataBase class

--- src/main/java/worksapce/eliana/DBbuilder/objects/DataBase.java
+++ src/main/java/worksapce/eliana/DBbuilder/objects/DataBase.java
@@ -1,4 +1,20 @@
 package worksapce.eliana.DBbuilder.objects;
 
+import java.util.Map;
+
 public class DataBase {
+
+    private String name;
+    private Map<String, Table> dataBase;
+
+    public DataBase(String name)
+    {
+        this.name=name;
+    }
+
+    public DataBase(String name, Map<String, Table> dataBase)
+    {
+        this.name=name;
+        this.dataBase=dataBase;
+    }
 }
